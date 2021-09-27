aee483a162bf926fe50201d166fa410e8f7ba2dc
Gowtham <gowtham758550@gmail.com>
2021-09-27 09:29:32 +0530

    added expense, advance and history view in reports component

--- src/app/components/reports/view/view.component.ts
+++ src/app/components/reports/view/view.component.ts
@@ -31,30 +31,48 @@ export class ViewComponent implements OnInit {
       this.report = res
       this.advanceList = this.report['advanceList']
       this.expenseList = this.report['expenseList']
-      // console.log(this.report)
-    })
-
-
-    if (this.advanceList.length === 0) {
-      this.trip.getalladvance(this.obj.email).subscribe(res => [
-        this.allAdvance = res
-      ])
-      this.expenseCount = this.allAdvance.length
-      this.isNoAdvance = true
-    } else {
-      this.isNoAdvance = true
-    }
     
-    if (this.expenseList.length === 0) {
-      this.expenseCount = 0
-      this.isNoExpense = true
-    } else {
-      this.http.get(`http://localhost:3000/api/getallexpenses/${this.obj.email}`).subscribe(res => {
-        this.allExpenses = res
-      })
-      this.isNoAdvance = false
-    }
+      if (this.expenseList.length === 0) {
+        this.isNoExpense = true
+      } else {
+        this.isNoExpense = false
+        const tempList1: Object[] = []
+        for (let i = 0; i < this.expenseList.length; i++) {
+          this.http.get(`http://localhost:3000/api/getexpense/${this.obj.email}/${this.expenseList[i]['expenseId']}`,).subscribe(res => {
+            tempList1.push(res)
+            // console.log(res)
+          })
+        }
+        this.expenseInReport = tempList1
+      }
+
+      if (this.advanceList.length === 0) {
+        this.isNoAdvance = true
+      } else {
+        this.isNoAdvance = false
+        const tempList2: Object[] = []
+        for (let i = 0; i < this.advanceList.length; i++) {
+          // console.log(this.advanceList[i]['advanceId'])
+          this.trip.getadvance(this.advanceList[i]['advanceId']).subscribe(res => {
+            tempList2.push(res)
+          })
+        }
+        this.advanceInReport = tempList2
+        // console.log(this.advanceInReport)
+      }
+    })
+    this.trip.getalladvance(this.obj.email).subscribe(res => [
+      this.allAdvance = res,
+      this.advanceCount = this.allAdvance.length
+    ])
+    this.http.get(`http://localhost:3000/api/getallexpenses/${this.obj.email}`).subscribe(res => {
+      this.allExpenses = res,
+      this.expenseCount = this.allExpenses.length
+    })
   }
+
+  expenseInReport:any
+  advanceInReport:any
 
   allAdvance:any
   allExpenses:any
@@ -148,9 +166,10 @@ viewtrip(email: any):void{
       amount: this.allExpenses[index]['amount']
     }
     this.reportService.addExpense(updateData).subscribe(res => {
-      // console.log(res)
+      console.log(res)
     })
     this.ngOnInit()
+    window.location.reload()
   }
 
   addAdvance(index:number) {
@@ -163,6 +182,7 @@ viewtrip(email: any):void{
       console.log(res)
     })
     this.ngOnInit()
+    window.location.reload()
   }
  
 
@@ -176,5 +196,7 @@ viewtrip(email: any):void{
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
   }
 
-
+  navigateToReports() {
+    this.router.navigate(['/reports'])
+  }
 }
